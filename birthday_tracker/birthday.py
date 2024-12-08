c600edd1e28c6c4d7b222fb3b1e27b3c8f25d04b
Ondrej Vondra <mrlucker518@gmail.com>
2024-12-08 19:36:54 +0100

    Improved days calculation

--- birthday_tracker/birthday.py
+++ birthday_tracker/birthday.py
@@ -1,30 +1,29 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class Birthday:
     def __init__(self, birth_date):
         self.birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
+        self.current_date = datetime.combine(datetime.now().date(), datetime.min.time())
 
     def _get_next_birthday(self):
-        now = datetime.now()
-        next_birthday = self.birth_date.replace(year=now.year)
-        if next_birthday < now:
-            next_birthday = next_birthday.replace(year=now.year + 1)
+        next_birthday = self.birth_date.replace(year=self.current_date.year)
+        if next_birthday < self.current_date:
+            next_birthday = next_birthday.replace(year=self.current_date.year + 1)
         return next_birthday
     
     def _get_last_birthday(self):
-        now = datetime.now()
-        last_birthday = self.birth_date.replace(year=now.year)
-        if last_birthday > now:
-            last_birthday = last_birthday.replace(year=now.year - 1)
+        last_birthday = self.birth_date.replace(year=self.current_date.year)
+        if last_birthday > self.current_date:
+            last_birthday = last_birthday.replace(year=self.current_date.year - 1)
         return last_birthday
 
     def get_progress(self):
-        now = datetime.now()
         next_birthday = self._get_next_birthday()
         last_birthday = self._get_last_birthday()
         
         year_span = next_birthday.timestamp() - last_birthday.timestamp()
-        elapsed = now.timestamp() - last_birthday.timestamp()
+        elapsed = self.current_date.timestamp() - last_birthday.timestamp()
         
         return min(elapsed / year_span, 1)
     
@@ -72,31 +71,24 @@ class Birthday:
 
     def get_birthday_day(self):
         next_birthday = self._get_next_birthday()
-        return (next_birthday - datetime.now()).days
+        return (next_birthday - self.current_date).days
 
     def get_total_years(self):
-        now = datetime.now()
-        return now.year - self.birth_date.year - ((now.month, now.day) < (self.birth_date.month, self.birth_date.day))
+
+        return self.current_date.year - self.birth_date.year - ((self.current_date.month, self.current_date.day) < (self.birth_date.month, self.birth_date.day))
 
     def get_total_months(self):
-        now = datetime.now()
-        years_diff = now.year - self.birth_date.year
-        months_diff = now.month - self.birth_date.month
+        years_diff = self.current_date.year - self.birth_date.year
+        months_diff = self.current_date.month - self.birth_date.month
         total_months = years_diff * 12 + months_diff
-        if now.day < self.birth_date.day:
+        if self.current_date.day < self.birth_date.day:
             total_months -= 1
         return total_months
-    
+
     def get_total_days(self):
-        now = datetime.now()
-        if now.day < self.birth_date.day:
-            previous_month = now.replace(day=1) - timedelta(days=1)
-            days = (previous_month.day - self.birth_date.day) + now.day
-        else:
-            days = now.day - self.birth_date.day
-        
-        return days
+        months_difference = relativedelta(self.current_date, self.birth_date)
+        remaining_days = (self.current_date - (self.birth_date + relativedelta(months=months_difference.months))).days
+        return remaining_days
         
     def is_birthday_day(self):
-        now = datetime.now()
-        return (now.month, now.day) == (self.birth_date.month, self.birth_date.day)
+        return (self.current_date.month, self.current_date.day) == (self.birth_date.month, self.birth_date.day)
